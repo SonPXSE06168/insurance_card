e9c8325194ba9e189ab310b5caab668655296cb3
SonPXSE06168 <sonpxse06168@fpt.edu.vn>
2022-06-13 12:20:04 +0700

    commit
    edit main

--- src/main/java/insurance_card/InsuranceCardApplication.java
+++ src/main/java/insurance_card/InsuranceCardApplication.java
@@ -1,29 +1,12 @@
-<<<<<<<< HEAD:src/main/java/insurance_card/InsuranceCardApplication.java
 package insurance_card;
-
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InsuranceCardApplication {
-	
+
 	public static void main(String[] args) {
-        SpringApplication.run(InsuranceCardApplication.class, args);
-    }
+		SpringApplication.run(InsuranceCardApplication.class, args);
+	}
 }
-========
-package insurance_card;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class InsuranceCardApplication {
-	
-	public static void main(String[] args) {
-        SpringApplication.run(InsuranceCardApplication.class, args);
-    }
-}
->>>>>>>> main:insurance_card/src/main/java/insurance_card/InsuranceCardApplication.java
